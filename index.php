527b4925e2b29b7665379bb3780b21338fa1535e
Moh. Ikbal Hadi Permana <moh.ikbalhp@gmail.com>
2021-09-26 09:40:21 +0700

    First Commit

---
+++ index.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio - Moh. Ikbal Hadi Permana</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <div id="home"></div>
+    <div id="header">
+        <div id="header-foto"></div>
+        <div id="header-text" class="text-name">Moh Ikbal Hadi Permana</div>
+        <div id="header-text" class="text-name2">- Design Graphics Lover -</div>
+    </div>
+    
+    <div id="nav-bar">
+        <div id="navbar-content">
+            <a href="#home"><div id="menu">HOME</div></a>
+            <a href="#link-about"><div id="menu">ABOUT</div></a>
+            <a href="#link-portfolio"><div id="menu">PORTFOLIO</div></a>
+            <a href="#link-blog"><div id="menu">BLOG</div></a>
+            <a href="#link-team"><div id="menu">TEAM</div></a>
+            <a href="#link-contact"><div id="menu">CONTACT</div></a>
+        </div>
+    </div>
+
+    <div id="link-about"></div>
+    <div id="about">
+        <div id="content">
+            <center><h1>About Me</h1></center>
+            <center><h4>I Love Design Graphics, Travelling, Hiking, Soccer, Futsal, etc.</h4></center>
+            <div id="foto">
+                <img src="img/foto-profile.jpg" alt="">
+            </div>
+            <div id="about-isitext">
+                Perkenalkan, saya Moh. Ikbal Hadi Permana, sering dipanggil Ikbal. Tapi, teman-teman dekat saya, memanggil saya dengan panggilan 'Munez'.
+                Munez? Kenapa Munez? Munez diambil dari nama pemeran di sebuah Film, yang berjudul Goal! (2007).
+                Film tersebut menceritakan tentang perjalanan seorang anak asal Meksiko yang tinggal di Amerika. Anak tersebut
+                bermimpi untuk menjadi pemain Sepak Bola Proffesional. <br>
+                ____________  
+            </div>
+        </div>
+    </div>
+
+    <div id="link-portfolio"></div>
+    <div id="portfolio">
+        <h1>My Portfolio</h1>
+        <div id="port-content">
+            <div id="foto-portfolio">
+                <img src="img/foto-profile.jpg" alt="">
+                <!-- <img src="img/foto-profile.jpg" alt="">
+                <img src="img/foto-profile.jpg" alt="">
+                <img src="img/foto-profile.jpg" alt="">
+                <img src="img/foto-profile.jpg" alt=""> -->
+            </div>
+            <div id="foto-portfolio">
+                <img src="img/foto-profile-1.jpg" alt="">
+            </div>
+            <div id="foto-portfolio">
+                <img src="img/foto-profile-2.jpg" alt="">
+            </div>
+            <div id="foto-portfolio">
+                <img src="img/foto-profile-3.jpg" alt="">
+            </div>
+                        <div id="foto-portfolio">
+                <img src="img/foto-profile-4.jpg" alt="">
+            </div>
+        </div>
+    </div>
+
+    <div id="link-blog"></div>
+    <div id="blog">
+        <h1>My Blog</h1>
+        <div id="blog-content">
+            <div id="blog-isi">
+                <div id="blog-foto">
+                    <img src="img/foto-profile.jpg" alt="">
+                </div>
+                <div id="blog-desc">
+                    20 Juni 2021 <br>
+                    <b>MT. Sagara, Garut</b>
+                </div>
+            </div>
+            <div id="blog-isi">
+                <div id="blog-foto">
+                    <img src="img/foto-profile-1.jpg" alt="">
+                </div>
+                <div id="blog-desc">
+                    17 Agustus 2018 <br>
+                    <b>MT. Guntur, Garut</b>
+                </div>
+            </div>
+            <div id="blog-isi">
+                <div id="blog-foto">
+                    <img src="img/foto-profile-3.jpg" alt="">
+                </div>
+                <div id="blog-desc">
+                    17 Agustus 2018 <br>
+                    <b>MT. Ciremai, Cirebon</b>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="link-team"></div>
+    <div id="team">
+        <center><h1>My Team</h1></center>
+        <center><h4>This is Our Perfect Team in The World</h4></center>
+        <div id="team-content">
+            <div id="team-foto">
+                <img src="img/team-1.jpg" alt="">
+            </div>
+            <div id="team-foto">
+                <img src="img/team-2.jpg" alt="">
+            </div>
+            <div id="team-foto">
+                <img src="img/team-3.jpg" alt="">
+            </div>
+            <div id="team-foto">
+                <img src="img/team-4.jpg" alt="">
+            </div>
+        </div>
+        <div id="desc-content">
+            <div id="team-desc">
+                <b>Moh. Ikbal Hadi Permana</b> <br>
+                <i>UI/UX Designer</i>
+            </div>
+
+            <div id="team-desc">
+                <b>Ariya Duta</b> <br>
+                <i>Software Engineering</i>
+            </div>
+
+            <div id="team-desc">
+                <b>Fikri Ishlah Azhar Haq</b> <br>
+                <i>Front-End Developer</i>
+            </div>
+
+            <div id="team-desc">
+                <b>Jabal Syukur</b> <br>
+                <i>Back-End Developer</i>
+            </div>
+        </div>
+    </div>
+
+    <div id="link-contact"></div>
+    <div id="contact">
+        <div id="contact-content" class="float-left p20">
+            <h1 class="fs">Contact Us</h1> <br><br>
+            <p class="fs10">moh.ikbalhp@gmail.com <br> 
+            +62 821-2650-0588 <br>
+            Soreang-Banjaran, Kab. Bandung <br>
+            Indonesia <br>
+            _____
+            </p>
+
+            <img src="img/medsos-icon.png" alt="">
+        </div>
+        <div id="contact-content2" class="float-right p2">
+            <h4>Any Questions? Let's do that!</h4>
+            <input type="text" placeholder="Enter Your Name">
+            <input type="text" placeholder="Enter Your Email Address">
+            <textarea rows="0" cols="0" placeholder="Enter Your Message"></textarea>
+            <input type="submit">
+        </div>
+    </div>
+</body>
+</html>
